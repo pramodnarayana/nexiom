0930fc0b66fa874c537f9457f0297fed32c3aa4f
Pramod <pramod.narayana@gmail.com>
2026-02-08 09:11:49 +0530

    refactor: use public identity api for constants

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -29,7 +29,4 @@ export const getRequiredOwnerRoleId = getOwnerRoleId;
 export const getRequiredAdminRoleId = getAdminRoleId;
 export const getRequiredMemberRoleId = getMemberRoleId;
 
-export {
-  ALL_PERMISSIONS,
-  isSystemPermission,
-} from '@nexiom/identity/src/constants';
+export { ALL_PERMISSIONS, isSystemPermission } from '@nexiom/identity';
